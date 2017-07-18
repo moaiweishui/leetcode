279f3edcd1eaaf78470bea55950c60d007246828
LiuSS <liuyuanxiu1218@gmail.com>
2017-07-18 20:27:15 +0800

    Update src.cpp

--- 561. Array Partition I/src.cpp
+++ 561. Array Partition I/src.cpp
@@ -12,16 +12,17 @@ public:
 public:
     int arrayPairSum(vector<int>& nums) {
         int num_size = nums.size();
+        int res = 0;
         for(int i = 0; i < num_size - 1; i ++)
+        {
             for(int j = 0; j < num_size - 1 - i; j ++)
                 if(nums[j] > nums[j + 1])
                     exchange(nums[j], nums[j + 1]);
-        
-        int res = 0;
-        for (int i = 0; i < num_size; i = i + 2)
-        {
-            res += nums[i];
+            if(i % 2 != 0)
+                res += nums[num_size - i - 1]; 
         }
+        res += nums[0]; 
+        
         
         return res;
     }
