4b1648772e33abba3f4e5a6a8514874e30345063
LiuSS <liuyuanxiu1218@gmail.com>
2017-08-03 16:16:07 +0800

    Update src.cpp

--- 242. Valid Anagram/src.cpp
+++ 242. Valid Anagram/src.cpp
@@ -1,3 +1,4 @@
+/**************** Solution 1 ***************/
 class Solution {
 public:
     bool isAnagram(string s, string t) {
@@ -13,5 +14,24 @@ public:
             t_ms.insert(t[i]);
         
         return s_ms == t_ms;
+    }
+};
+
+/**************** Solution 2 ***************/
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int s_size = s.length();
+        int t_size = t.length();
+        if(s_size!=t_size)
+            return false;
+
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        
+        if(s.compare(t) == 0)
+            return true;
+        else
+            return false;
     }
 };
