a7da78027b415ba1689c7c4dfae9eb38839a839a
LiuSS <liuyuanxiu1218@gmail.com>
2017-08-03 15:34:41 +0800

    Create src.cpp

---
+++ 242. Valid Anagram/src.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int s_size = s.length();
+        int t_size = t.length();
+        if(s_size!=t_size)
+            return false;
+        multiset<char> s_ms;
+        multiset<char> t_ms;
+        for(int i = 0; i < s_size; i++)
+            s_ms.insert(s[i]);
+        for(int i = 0; i < t_size; i++)
+            t_ms.insert(t[i]);
+        
+        return s_ms == t_ms;
+    }
+};
