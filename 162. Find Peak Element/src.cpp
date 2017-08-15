01a218bd36ea1925d0a6bdba0c7e40953c78d948
LiuSS <liuyuanxiu1218@gmail.com>
2017-08-15 18:30:57 +0800

    Create src.cpp

---
+++ 162. Find Peak Element/src.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int len = nums.size();
+        if(len == 1)
+            return 0;
+        else if(len == 2)
+            return nums[0] > nums[1] ? 0 : 1;
+        
+        if(nums[0] > nums[1])
+            return 0;
+        else if(nums[len-1] > nums[len - 2])
+            return len - 1;
+        
+        for(int i = 1; i < len-1; i++)
+        {
+            if(nums[i] > nums[i-1] && nums[i] > nums[i+1])
+                return i;
+        }
+        
+    }
+};
