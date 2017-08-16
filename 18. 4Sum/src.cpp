27dbc5a37c87f9982cf51bef7fd631d1cdcb4330
LiuSS <liuyuanxiu1218@gmail.com>
2017-08-16 10:59:46 +0800

    Update src.cpp

--- 18. 4Sum/src.cpp
+++ 18. 4Sum/src.cpp
@@ -79,3 +79,73 @@ public:
         return res;
     }
 };
+
+
+/******************* Solution 1 ************************/
+class Solution {
+public:
+    vector<vector<int>> twoSum(vector<int>& nums, int start, int end, int target)
+    {
+        int left = start;
+        int right = end;
+
+        set<vector<int>> res_set;
+        vector<int> tmp;
+
+        while (left != right)
+        {
+            if (nums[left] + nums[right] == target)
+            {
+                tmp.clear();
+                tmp.push_back(nums[left]);
+                tmp.push_back(nums[right]);
+                res_set.insert(tmp);
+                if (left == right - 1)
+                    break;
+                left++;
+                right--;
+            }
+            else if (nums[left] + nums[right] > target)
+            {
+                right--;
+            }
+            else if (nums[left] + nums[right] < target)
+            {
+                left++;
+            }
+        }
+        vector<vector<int>> res(res_set.begin(), res_set.end());
+        return res;
+    }
+    
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int m = nums.size();
+        vector<vector<int>> res;
+        if(m < 4)
+            return res;
+
+        
+        sort(nums.begin(), nums.end());
+        
+        set<vector<int>> tmpset;
+        
+        for(int i = 0; i < m - 3; i++)
+        {
+            for (int j = i + 1; j < m - 2; j++)
+            {
+                vector<vector<int>> tmpv = twoSum(nums, j+1, m-1, target-nums[i]-nums[j]);
+                for (int k = 0; k < tmpv.size(); k++)
+                {
+                    tmpv[k].insert(tmpv[k].begin(), nums[j]);
+                    tmpv[k].insert(tmpv[k].begin(), nums[i]);
+                    tmpset.insert(tmpv[k]);
+                }           
+            }    
+        }
+        
+        res.assign(tmpset.begin(), tmpset.end());
+        
+        return res;
+        
+    }
+};
