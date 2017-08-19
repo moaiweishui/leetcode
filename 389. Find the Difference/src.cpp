6f4506b6fd7373800836aafecfb69914f0bffc13
LiuSS <liuyuanxiu1218@gmail.com>
2017-08-19 23:18:41 +0800

    1: 20%  2: 65%

--- 389. Find the Difference/src.cpp
+++ 389. Find the Difference/src.cpp
@@ -1,3 +1,4 @@
+/********************** Solution 1 ****************************/
 class Solution {
 public:
     char findTheDifference(string s, string t) {
@@ -11,5 +12,28 @@ public:
         }
         
         return t[i];
+    }
+};
+
+/********************** Solution 2 ****************************/
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        
+        vector<int> count(26, 0);
+        
+        for (int i = 0; i < s.length(); i++)
+            count[s[i] - 97] ++;
+
+        for (int i = 0; i < t.length(); i++)
+        {
+            count[t[i] - 97] --;
+            if (count[t[i] - 97] == -1)
+            {
+                return t[i];
+            }
+            
+        }
+        
     }
 };
