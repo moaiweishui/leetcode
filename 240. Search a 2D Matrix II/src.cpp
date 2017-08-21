4e8873a7a84e82ca63180ea62eb73b2a47f271b7
LiuSS <liuyuanxiu1218@gmail.com>
2017-08-21 15:32:09 +0800

    1: 5% 2: 57%

--- 240. Search a 2D Matrix II/src.cpp
+++ 240. Search a 2D Matrix II/src.cpp
@@ -41,3 +41,38 @@ public:
             
     }
 };
+
+/**************** Solution 2 ******************/
+class Solution {
+public:
+    bool searchMatrixWithPos(vector<vector<int>>& matrix, int target, int start_r, int end_c)
+    {
+        if(matrix[start_r][end_c] == target)
+        {
+            return true;
+        }
+        else if(matrix[start_r][end_c] > target)
+        {
+            if (end_c == 0)
+                return false;
+            return searchMatrixWithPos(matrix, target, start_r, end_c - 1);
+        }
+        else
+        {
+            if (start_r == matrix.size() - 1)
+                return false;
+            return searchMatrixWithPos(matrix, target, start_r + 1, end_c);
+        }
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int r = matrix.size();
+        if(r == 0)
+            return false;
+        int c = matrix[0].size();
+        if(c == 0)
+            return false;
+
+        return searchMatrixWithPos(matrix, target, 0, c - 1);
+            
+    }
+};
