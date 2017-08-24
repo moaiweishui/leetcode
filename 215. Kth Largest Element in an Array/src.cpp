fa526f361d50f1139deb327f6649f36452a6ffb7
LiuSS <liuyuanxiu1218@gmail.com>
2017-08-24 09:47:54 +0800

    1: 61% 2: 57%

--- 215. Kth Largest Element in an Array/src.cpp
+++ 215. Kth Largest Element in an Array/src.cpp
@@ -8,3 +8,19 @@ public:
 };
 
 /************* Solution 2 ***************/
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        
+        priority_queue<int> q(nums.begin(), nums.end());
+        
+        for(int i = 0; i < k; i++)
+        {
+            if(i == k - 1)
+                return q.top();
+            else
+                q.pop();
+        }
+        
+    }
+};
