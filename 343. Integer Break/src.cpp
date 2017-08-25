60b59d69b5721521a6a1f205003f978852df560e
LiuSS <liuyuanxiu1218@gmail.com>
2017-08-25 09:38:27 +0800

    33%

---
+++ 343. Integer Break/src.cpp
@@ -0,0 +1,25 @@
+//DP
+class Solution {
+public:
+    int integerBreak(int n) {
+        if(n == 2)
+            return 1;
+        
+        vector<int> res(n + 1, 0);
+        res[1] = 0;
+        res[2] = 1;
+        int temp;
+        int maxproduct = 0;
+        for(int i = 3; i <= n; i++)
+        {
+            for(int j = 1; j < i; j++)
+            {
+                temp = j * max(i - j, res[i - j]);
+                if(temp > maxproduct)
+                    maxproduct = temp;
+            }
+            res[i] = maxproduct;
+        }
+        return res[n];
+    }
+};
