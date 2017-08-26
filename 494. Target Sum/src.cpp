c493fa12ddd3cef30efa6411dcf3d129c63fa96e
LiuSS <liuyuanxiu1218@gmail.com>
2017-08-26 09:42:19 +0800

    Create src.cpp

---
+++ 494. Target Sum/src.cpp
@@ -0,0 +1,37 @@
+/******************* Solution 1 ********************/
+class Solution {
+public:
+    int findTargetSumWaysWithLen(vector<int>& nums, int len, int S, vector<int>& sumv)
+    {
+        if(len == 0)
+            return 0;
+        else if(len == 1)
+        {
+            if(S == 0)
+                return nums[0]==S ? 2 : 0;
+            else
+                return abs(nums[0])==abs(S) ? 1 : 0;
+        }
+        else if(S > sumv[len - 1] || S < -sumv[len - 1])
+        {
+            return 0;
+        }
+        else
+        {
+            return findTargetSumWaysWithLen(nums, len - 1, S + nums[len - 1], sumv) + findTargetSumWaysWithLen(nums, len - 1, S - nums[len - 1], sumv);
+        }
+    }
+    
+    int findTargetSumWays(vector<int>& nums, int S) {    
+        vector<int> sumv(nums.size(), 0);
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(i == 0)
+                sumv[i] = abs(nums[i]);
+            else
+                sumv[i] = abs(nums[i]) + sumv[i - 1];
+        }
+        
+        return findTargetSumWaysWithLen(nums, nums.size(), S, sumv);      
+    }
+};
