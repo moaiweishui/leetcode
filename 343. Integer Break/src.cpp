728278c3e1dfd9c148e7afcd74f2ac65d9e16066
LiuSS <liuyuanxiu1218@gmail.com>
2017-08-28 09:28:04 +0800

    33%

--- 343. Integer Break/src.cpp
+++ 343. Integer Break/src.cpp
@@ -12,7 +12,7 @@ public:
         int maxproduct = 0;
         for(int i = 3; i <= n; i++)
         {
-            for(int j = 1; j < i; j++)
+            for(int j = 1; j <= i/2; j++)
             {
                 temp = j * max(i - j, res[i - j]);
                 if(temp > maxproduct)
