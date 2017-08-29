08907896433406d243e64dd6727fbce0db88aa12
LiuSS <liuyuanxiu1218@gmail.com>
2017-08-29 14:55:19 +0800

    Update src.cpp

--- 413. Arithmetic Slices/src.cpp
+++ 413. Arithmetic Slices/src.cpp
@@ -85,3 +85,42 @@ public:
 
     }
 };
+
+/********************** Solution 3 **********************/
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& A) {
+       
+        int len = A.size();
+        if(len < 3)
+            return 0;
+            
+        vector<int> mem(len, 0);
+        
+        if(A[0] - A[1] == A[1] - A[2])
+            mem[2] = 1;
+        
+        int step;
+        int p, depth;
+        for(int i = 3; i < len; i++)
+        {
+            step = A[i] - A[i - 1];
+            p = i - 2;
+            while(p >= 0)
+            {
+                if(A[p + 1] - A[p] != step)
+                    break;
+                else
+                    p--;
+            }
+            
+            depth = i - 1 - p;
+            if(depth < 2)
+                mem[i] = mem[i - 1];
+            else
+                mem[i] = depth - 1 + mem[i - 1];
+        }
+
+        return mem[len - 1];
+    }
+};
