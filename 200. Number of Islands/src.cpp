0fd4535abb0d8f8ded09d90a4c91d172a97b538f
LiuSS <liuyuanxiu1218@gmail.com>
2017-09-07 20:31:59 +0800

    1、2都是26%

--- 200. Number of Islands/src.cpp
+++ 200. Number of Islands/src.cpp
@@ -1,3 +1,4 @@
+/********************** Solution 1 *********************/
 class Solution {
 public:
     int countTag(vector<vector<int>>& tag)
@@ -99,5 +100,106 @@ public:
         }
         
         return countTag(tag);
+    }
+};
+
+/********************** Solution 2 *********************/
+//基本和S1一样，只是计数过程直接在主循环内完成
+class Solution {
+public:  
+    int numIslands(vector<vector<char>>& grid) {
+        int r = grid.size();
+        if(r == 0)
+            return 0;
+        
+        int c = grid[0].size();
+        
+        if(c == 0)
+            return 0;
+        
+        vector<vector<int>> tag(r, vector<int>(c, 0));
+        
+        int max = INT_MIN;
+        int cnt = 0;
+        
+        int res = 0;
+        
+        for(int i = 0; i < r; i++)
+        {
+            for(int j = 0; j < c; j++)
+            {
+                if(i == 0 && j == 0)
+                {
+                    if(grid[i][j] == '1')
+                    {
+                        tag[i][j] = 1;
+                        res++;
+                        cnt++;
+                    }
+                }
+                else if(i == 0)
+                {
+                    if(grid[i][j] == '1' && grid[i][j - 1] != '1')
+                    {
+                        cnt++;
+                        res++;
+                        tag[i][j] = cnt;
+                    }
+                    else if(grid[i][j] == '1' && grid[i][j - 1] == '1')
+                    {
+                        tag[i][j] = tag[i][j - 1];
+                    }
+                }
+                else if(j == 0)
+                {
+                    if(grid[i][j] == '1' && grid[i - 1][j] != '1')
+                    {
+                        cnt++;
+                        res++;
+                        tag[i][j] = cnt;
+                    }
+                    else if(grid[i][j] == '1' && grid[i - 1][j] == '1')
+                    {
+                        tag[i][j] = tag[i - 1][j];
+                    }
+                }
+                else
+                {
+                    if(grid[i][j] == '1' && grid[i][j - 1] != '1' && grid[i - 1][j] != '1')
+                    {
+                        cnt++;
+                        res++;
+                        tag[i][j] = cnt;
+                    }
+                    else if(grid[i][j] == '1' && grid[i][j - 1] != '1')
+                    {
+                        tag[i][j] = tag[i - 1][j];
+                    }
+                    else if(grid[i][j] == '1' && grid[i - 1][j] != '1')
+                    {
+                        tag[i][j] = tag[i][j - 1];
+                    }
+                    else if(grid[i][j] == '1')
+                    {
+                        if(tag[i - 1][j] != tag[i][j - 1])
+                        {
+                            int tmp = tag[i - 1][j];
+                            tag[i][j] = tmp;
+                            for(int k = 0; k < r; k++)
+                                for(int s = 0; s < c; s++)
+                                    if(tag[k][s] == tag[i][j - 1])
+                                        tag[k][s] = tmp;
+                            res--;
+                        }
+                        else
+                        {
+                            tag[i][j] = tag[i][j - 1];
+                        }
+                        
+                    }
+                }
+            }
+        }
+        return res;
     }
 };
