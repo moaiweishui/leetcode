ed5b5ad0e33763fd22def9488021eaa8c67bbf41
LiuSS <liuyuanxiu1218@gmail.com>
2018-03-20 21:48:17 +0800

    86.38%

--- 3. Longest Substring Without Repeating Characters/Solution.java
+++ 3. Longest Substring Without Repeating Characters/Solution.java
@@ -19,3 +19,29 @@ class Solution {
         return maxLength;
     }
 }
+
+/**************************** Solution 2 ********************************/
+// 86.38%
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int maxLength = 0;
+
+        //用来记录字符出现的位置
+        int[] pos = new int[128];
+        // 从1开始
+        int start = 1;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(pos[s.charAt(i)] != 0){
+                //该字符在之前出现过
+                if(pos[s.charAt(i)] >= start){
+                    start = pos[s.charAt(i)] + 1;
+                }
+            }
+            maxLength = Math.max(i + 1 - start + 1, maxLength);
+            pos[s.charAt(i)] = i + 1;
+        }
+
+        return maxLength;
+    }
+}
