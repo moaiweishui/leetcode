18d4a41fce2e54441feafb167d34d866fd4e201e
LiuSS <liuyuanxiu1218@gmail.com>
2018-03-26 20:34:56 +0800

    23.95%

---
+++ 98. Validate Binary Search Tree/Solution.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if(root == null) return true;
+
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode pre = null;
+        TreeNode curr = root;
+
+        while(curr != null || !st.isEmpty()){
+            while(curr != null){
+                //沿左下遍历到底
+                st.push(curr);
+                curr = curr.left;
+            }
+
+            curr = st.pop();
+            if(pre != null && curr.val <= pre.val) return false;
+            pre = curr;
+            //跳到右节点
+            curr = curr.right;
+        }
+
+        return true;
+    }
+}
