89850da5c30f42689e20e4776521a83401fb72b6
LiuSS <liuyuanxiu1218@gmail.com>
2018-04-02 22:21:19 +0800

    88.89% 11.87%

--- 102. Binary Tree Level Order Traversal/src.cpp
+++ 102. Binary Tree Level Order Traversal/src.cpp
@@ -1,3 +1,4 @@
+//////// 88.89% /////////
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -9,58 +10,63 @@
  */
 class Solution {
 public:
-    struct TreeNodeWithLevel{
-	    TreeNode* node;
-	    int level;
-	    TreeNodeWithLevel(TreeNode* node) :node(node), level(NULL){}
-    };
-    
     vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>> res;
-        if(!root)
-            return res;
-        
-        TreeNodeWithLevel* n0 = new TreeNodeWithLevel(root);
-	    n0->level = 1;
+        if (root == NULL) return res;
+        vector<TreeNode*> vec;
+        vec.push_back(root);
+        int curr = 0;
+        int end = vec.size();
+        while (curr < vec.size()){
+            end = vec.size();
+            vector<int> temp_vec;
+            while (curr != end){
+                //开始这一层节点的遍历
+                temp_vec.push_back(vec[curr]->val);
+                if (vec[curr]->left != NULL) vec.push_back(vec[curr]->left);
+                if (vec[curr]->right != NULL) vec.push_back(vec[curr]->right);
+                curr++;
+            }
+            res.push_back(temp_vec);
+        }
 
-	    queue<TreeNodeWithLevel*> q;
-
-	    q.push(n0);
-        
-        int last_level = 1;
-	    vector<int> tmp;
-        
-        while (!q.empty())
-	    {
-		    cout << q.front()->node->val <<'-'<< q.front()->level<<endl;
-
-		    if (q.front()->level == last_level)
-		    {
-			    tmp.push_back(q.front()->node->val);
-		    }
-		    else
-		    {
-			    res.push_back(tmp);
-			    tmp.clear();
-			    tmp.push_back(q.front()->node->val);
-			    last_level = q.front()->level;
-		    }
-
-		    if (q.front()->node->left)
-		    {
-			    TreeNodeWithLevel* tmp = new TreeNodeWithLevel(q.front()->node->left);
-			    tmp->level = q.front()->level + 1;
-			    q.push(tmp);
-		    }
-		    if (q.front()->node->right)
-		    {
-			    TreeNodeWithLevel* tmp = new TreeNodeWithLevel(q.front()->node->right);
-			    tmp->level = q.front()->level + 1;
-			    q.push(tmp);
-		    }
-		    q.pop();
-	    }
-	    res.push_back(tmp);
         return res;
+    }
+};
+
+/////////////////////////// 11.87% ///////////////////////////////
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+    if (root == NULL) return res;
+    
+    queue<TreeNode*> qa, qb;
+    qa.push(root);
+
+    TreeNode* node;
+    while (!qa.empty()){
+        vector<int> temp_vec;
+        while (!qa.empty()){
+            node = qa.front();
+            temp_vec.push_back(node->val);
+            if (node->left != NULL) qb.push(node->left);
+            if (node->right != NULL) qb.push(node->right);
+            qa.pop();
+        }
+        qa.swap(qb);
+        res.push_back(temp_vec);
+    }
+
+    return res;
     }
 };
