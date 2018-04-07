85e2b909967b448f41f0a6746303f1c7388b9128
LiuSS <liuyuanxiu1218@gmail.com>
2018-04-07 11:34:17 +0800

    1: 61%  2: 57%  3: 77.01%  4: 23.18%

--- 215. Kth Largest Element in an Array/src.cpp
+++ 215. Kth Largest Element in an Array/src.cpp
@@ -24,3 +24,58 @@ public:
         
     }
 };
+
+/************* Solution 3 ***************/
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        make_heap(nums.begin(), nums.end(), less<int>());
+        int res;
+        for (int i = 1; i <= k; i++){
+            pop_heap(nums.begin(), nums.end());
+            res = nums[nums.size() - 1];
+            nums.pop_back();
+        }
+        return res;
+    }
+};
+
+/************* Solution 4 ***************/
+class Solution {
+public:
+    int partition(vector<int>& nums, int left, int right){
+        if (left > right) return -1;
+        int base_index = right;
+        int parted_index = left - 1;
+        for (int i = left; i < right; i++){
+            if (nums[i] < nums[base_index]){
+                parted_index++;
+                if (parted_index != i)
+                    swap(nums[parted_index], nums[i]);
+            }
+        }
+        parted_index++;
+        if (parted_index != right)
+            swap(nums[parted_index], nums[right]);
+        return parted_index;
+    }
+    
+    int findKthLargest(vector<int>& nums, int k) {
+        int target_index = nums.size() - k;
+        int left = 0;
+        int right = nums.size() - 1;
+        int index = partition(nums, left, right);
+        while (index != target_index){
+            if (index > target_index){
+                //在左边继续找
+                right = index - 1;
+                index = partition(nums, left, right);
+            }
+            else{
+                left = index + 1;
+                index = partition(nums, left, right);
+            }
+        }
+        return nums[index];
+    }
+};
