1810f1eedc3e606ce8ffaedf62f7ebb126553a39
LiuSS <liuyuanxiu1218@gmail.com>
2018-05-29 21:06:39 +0800

    40.48%

--- 110. Balanced Binary Tree/src.cpp
+++ 110. Balanced Binary Tree/src.cpp
@@ -9,42 +9,19 @@
  */
 class Solution {
 public:
-    bool isBalanced(TreeNode* root) {
-        unordered_map<TreeNode*, int> height_map;
+    int getHeight(TreeNode* node){
+        if (node == NULL) return 0;
 
-    stack<TreeNode*> st;
-    TreeNode* curr = root;
-    TreeNode* pre = NULL;
+        int left_h = getHeight(node->left);
+        if (left_h == -1) return -1;
+        int right_h = getHeight(node->right);
+        if (right_h == -1) return -1;
 
-    while (curr != NULL || !st.empty()){
-        while (curr != NULL){
-            st.push(curr);
-            curr = curr->left;
-        }
-
-        curr = st.top();
-        if (curr->right != NULL && curr->right != pre){
-            curr = curr->right;
-        }
-        else{
-            int l_height, r_height;
-            if (curr->left == NULL) l_height = 0;
-            else l_height = height_map[curr->left];
-
-            if (curr->right == NULL) r_height = 0;
-            else r_height = height_map[curr->right];
-
-            if (abs(l_height - r_height) > 1) return false;
-
-            height_map[curr] = max(l_height, r_height) + 1;
-
-            pre = curr;
-            curr = NULL;
-            st.pop();
-        }
-
+        if (abs(left_h - right_h) > 1) return -1;
+        else return max(left_h + 1, right_h + 1);
     }
-
-    return true;
+    
+    bool isBalanced(TreeNode* root) {
+        return getHeight(root) != -1;
     }
 };
