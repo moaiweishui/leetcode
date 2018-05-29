321a1e0e6d9d19c6ed27e438bffff8f74193cf18
LiuSS <liuyuanxiu1218@gmail.com>
2018-05-29 22:51:45 +0800

    99.26%

--- 121. Best Time to Buy and Sell Stock/src.cpp
+++ 121. Best Time to Buy and Sell Stock/src.cpp
@@ -1,63 +1,21 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int res = 0;
-        int buy;
-        bool have_stock = false;
-        
-        if(prices.size() < 2)
-            return 0;
-        else if(prices.size() == 2)
-        {
-            if((prices[1]-prices[0]) > 0)
-                return prices[1]-prices[0];
-            else
-                return 0;
-        }
-        
-        
-        
-        for(int i = 0; i < prices.size() - 1; i++)
-        {
-            if(!have_stock)
-            {
-                int max = prices[i];
-                for(int j = i + 1; j < prices.size(); j++)
-                {
-                    if(prices[j] > max)
-                        max = prices[j];
-                }
-                if(max > prices[i])
-                {
-                    buy = prices[i];
-                    res = max - prices[i];
-                    have_stock = true;
-                }
+        if(prices.size() < 2) return 0;
+        vector<int> min_v(prices.size(), 0);
+        int max_p = 0;
+        min_v[0] = prices[0];
+        for (int i = 1; i < min_v.size(); i++){
+            if (prices[i] < min_v[i - 1]){
+                min_v[i] = prices[i];
             }
-            else
-            {
-                if(prices[i] < buy)
-                {
-                    int max = prices[i];
-                    for(int j = i + 1; j < prices.size(); j++)
-                    {
-                        if(prices[j] > max)
-                            max = prices[j];
-                    }
-                    if(max - prices[i] > res)
-                    {
-                        buy = prices[i];
-                        res = max - prices[i];
-                        have_stock = true;
-                    }
+            else{
+                min_v[i] = min_v[i - 1];
+                if (prices[i] - min_v[i] > max_p){
+                    max_p = prices[i] - min_v[i];
                 }
             }
         }
-        
-        if(have_stock)
-            return res;
-        else
-            return 0;
-        
+        return max_p;
     }
 };
