aa91f516f35eb659af1d9070405e10acfbf5b8a9
LiuSS <liuyuanxiu1218@gmail.com>
2018-06-01 00:20:01 +0800

    91.98%

--- 111. Minimum Depth of Binary Tree/src.cpp
+++ 111. Minimum Depth of Binary Tree/src.cpp
@@ -1,54 +1,13 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
-    struct TreeNodeWithLevel{
-	    TreeNode* node;
-	    int level;
-	    TreeNodeWithLevel(TreeNode* node) :node(node), level(NULL){}
-    };
-    
     int minDepth(TreeNode* root) {
+        if(root == NULL) return 0;
         
-        if(!root)
-            return 0;
+        if(root->left == NULL) return minDepth(root->right) + 1;
+        if(root->right == NULL) return minDepth(root->left) + 1;
         
-        TreeNodeWithLevel* n0 = new TreeNodeWithLevel(root);
-	    n0->level = 1;
-
-	    queue<TreeNodeWithLevel*> q;
-
-	    q.push(n0);
-        
-        int last_level = 1;
-        int min_depth = 0x7FFFFFFF;
-        
-        while (!q.empty())
-	    {
-		    if (q.front()->node->left == NULL && q.front()->node->right == NULL && q.front()->level < min_depth)
-			    min_depth = q.front()->level;
-
-		    if (q.front()->node->left)
-		    {
-			    TreeNodeWithLevel* tmp = new TreeNodeWithLevel(q.front()->node->left);
-			    tmp->level = q.front()->level + 1;
-			    q.push(tmp);
-		    }
-		    if (q.front()->node->right)
-		    {
-			    TreeNodeWithLevel* tmp = new TreeNodeWithLevel(q.front()->node->right);
-			    tmp->level = q.front()->level + 1;
-			    q.push(tmp);
-		    }
-		    q.pop();
-	    }
-	    return min_depth;
+        int left_h = minDepth(root->left);
+        int right_h = minDepth(root->right);
+        return min(left_h + 1, right_h + 1);   
     }
 };
